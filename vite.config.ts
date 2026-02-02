51804238be3446500bb04ee75691647fe40bfa40
Taniel Rémi <taniel.remi@gmail.com>
2026-02-02 07:33:08 +0100

    :sparkles: build cjs

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
+import dtsPlugin from "vite-plugin-dts";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dtsPlugin({
+      tsconfigPath: resolve(__dirname, "tsconfig.build.json"),
+    }),
+  ],
   build: {
     copyPublicDir: false,
     emptyOutDir: true,
-    minify: false,
+    minify: true,
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
-      formats: ["es"],
+      fileName: (format) => `main.${format}.js`,
+      formats: ["es", "cjs"],
+    },
+    rolldownOptions: {
+      external: ["clsx", "react", "react-dom", "react/jsx-runtime"],
     },
   },
   resolve: {
